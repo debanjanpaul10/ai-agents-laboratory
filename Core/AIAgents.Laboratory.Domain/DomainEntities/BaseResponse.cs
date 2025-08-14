fda7b4ea1b1b63b42cb0c3e9459db90517b13479
Debanjan Paul <debanjanpaul10@gmail.com>
2025-08-15 02:07:57 +0530

    Update xml doc

--- Core/AIAgents.Laboratory.Domain/DomainEntities/BaseResponse.cs
+++ Core/AIAgents.Laboratory.Domain/DomainEntities/BaseResponse.cs
@@ -2,7 +2,7 @@
 //	<copyright file="BaseResponse.cs" company="Personal">
 //		Copyright (c) 2025 Personal
 //	</copyright>
-// <summary>The content moderation response DTO.</summary>
+// <summary>The base response DTO.</summary>
 // *********************************************************************************
 namespace AIAgents.Laboratory.Domain.DomainEntities;
 
