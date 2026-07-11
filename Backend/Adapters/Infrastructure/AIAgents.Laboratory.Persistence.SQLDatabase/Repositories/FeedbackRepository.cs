c92c79830f4647b953451f51d1be79185629b1f8
Debanjan Paul <debanjanpaul10@gmail.com>
2026-07-11 14:43:18 +0530

    Fixed cancellation token

--- Backend/Adapters/Infrastructure/AIAgents.Laboratory.Persistence.SQLDatabase/Repositories/FeedbackRepository.cs
+++ Backend/Adapters/Infrastructure/AIAgents.Laboratory.Persistence.SQLDatabase/Repositories/FeedbackRepository.cs
@@ -19,13 +19,13 @@ public sealed class FeedbackRepository(IUnitOfWork unitOfWork) : IFeedbackReposi
     )
     {
         var bugStatusEntity = await unitOfWork.Repository<BugItemStatusMappingEntity>()
-            .FirstOrDefaultAsync(predicate: status => status.StatusName == DatabaseConstants.NotStartedConstant && status.IsActive, cancellationToken)
+            .FirstOrDefaultAsync(predicate: status => status.StatusName == DatabaseConstants.NotStartedConstant && status.IsActive, cancellationToken: cancellationToken)
             .ConfigureAwait(false);
 
         bugReportData.BugStatusId = bugStatusEntity?.Id ?? 0;
 
         await unitOfWork.Repository<BugReportDataEntity>()
-            .AddAsync(entity: bugReportData, cancellationToken)
+            .AddAsync(entity: bugReportData, cancellationToken: cancellationToken)
             .ConfigureAwait(false);
 
         await unitOfWork.SaveChangesAsync(
